505d60bbd56c3adb2055c2ecbc86b2ae67e1226d
Jesse Junior <jejunior@support.connectionsAcademy.org>
2024-12-13 17:06:06 -0300

    nucrunch setup

--- ConwaysGame.Core/Class1.cs
+++ ConwaysGame.Core/Class1.cs
@@ -2,10 +2,16 @@
 {
 
     //
-
+    
 
     public class Class1
     {
+        public static void main()
+        {
+            var x = 20;
+            var y = 20;
 
+            bool[,] h = new bool[x, y];
+        }
     }
 }
---
+++ ConwaysGame.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using ConwaysGame.Core;
+
+namespace ConwaysGame.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Class1.main();
+        }
+    }
+}
