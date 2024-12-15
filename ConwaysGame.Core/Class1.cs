372e9130b2c670be0325ac17aa6e27a0479fbfd0
Jesse Junior <jejunior@support.connectionsAcademy.org>
2024-12-15 17:44:57 -0300

    wip

--- ConwaysGame.Core/Class1.cs
+++ ConwaysGame.Core/Class1.cs
@@ -1,17 +1,71 @@
-﻿namespace ConwaysGame.Core
+﻿
+
+/*
+ Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+Any live cell with two or three live neighbors lives on to the next generation.
+Any live cell with more than three live neighbors dies, as if by overpopulation.
+Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+ */
+
+//TODO: THINK OF SPARSE AND DENSE GRIDS
+//TODO: THINK AND TEST PARALLELISM, specially for final state we can share the state for trheads, 
+
+global using GameBoard = bool[,];
+global using LivePopulation = (int x, int y)[];
+
+namespace ConwaysGame.Core;
+public class GameBoardModule
 {
-
-    //
+    public static GameBoard Create(int x, int y) => new bool[x, y];
     
-
-    public class Class1
+    public static GameBoard Populate(GameBoard board, LivePopulation population)
     {
-        public static void main()
+        foreach (var (x, y) in population)
         {
-            var x = 20;
-            var y = 20;
+            board[x, y] = true;
+        }
+        return board;
+    }
 
-            bool[,] h = new bool[x, y];
+
+    public static int GetLiveNeighbors(GameBoard board, int x, int y)
+    {
+        int count = 0;
+        int rows = board.GetLength(0);
+        int cols = board.GetLength(1);
+        
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (i == 0 && j == 0) continue;
+                
+                int x1 = x + i;
+                int y1 = y + j;
+                if (x1 >= 0 && x1 < rows && y1 >= 0 && y1 < cols && board[x1, y1])
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void PrintBoard(bool[,] board)
+    {
+
+        int rows = board.GetLength(0);
+        int cols = board.GetLength(1);
+
+        Console.WriteLine("--------------------------------------------------------------------------");
+        for (int x = 0; x < rows; x++)
+        {
+            for (int y = 0; y < cols; y++)
+            {
+                Console.Write(board[x, y] ? "O" : ".");
+            }
+            Console.WriteLine();
         }
     }
 }
+
--- ConwaysGame.Tests/UnitTest1.cs
+++ ConwaysGame.Tests/UnitTest1.cs
@@ -1,13 +1,82 @@
+global using GameBoard = bool[,];
+global using LivePopulation = (int x, int y)[]; //TODO: duplicate global using
 using ConwaysGame.Core;
+using FluentAssertions;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ConwaysGame.Tests
 {
     public class UnitTest1
     {
+        //[Theory]
+        //[MemberData(nameof(boards))]
+        //public void CorrectNieghborNumber(GameBoard board, int expectedNeighbors)
+        //{
+        //    GameBoardModule.GetLiveNeighbors(board, 1, 1).Should().Be(expectedNeighbors);
+
+        //}
+
         [Fact]
-        public void Test1()
+        public void test()
         {
-            Class1.main();
+            
+            var x = GenerateAllCombinations(8)
+                .Select(k =>
+                {
+                    GameBoard board = new bool[3, 3];
+
+                    for (int i = 0; i < k.Length; i++)
+                    {
+                        var _i = i;
+
+                        if (_i == 4) _i++;
+
+                        var line = _i % 3;
+                        
+                        var column = _i - line;
+
+                        Console.WriteLine("line:" + line.ToString() + " column:" + column.ToString());
+
+                        board[line, column] = k[i];
+                    }
+
+
+                    return board;
+                })
+                .ToArray();
+
+            
+            
         }
+
+        private static IEnumerable<bool[]> GenerateAllCombinations(int length)
+        {
+            int totalCombinations = (int)Math.Pow(2, length);
+            
+            for (int i = 0; i < totalCombinations; i++)
+            {
+                bool[] combination = new bool[length];
+                for (int j = 0; j < length; j++)
+                {
+                    combination[j] = ((i >> j) & 1) == 1;
+                }
+                yield return combination;
+            }
+        }
+        
+
+
+        //public static IEnumerable<object[]> boards
+        //{
+        //    get
+        //    {
+        //        (bool[], int)[] CombinationsAndResults = 
+        //            GenerateAllCombinations(8)
+
+        //            .Select(x => (x, x.Count(cell => cell))).ToArray();
+
+
+        //    }
+        //}
     }
 }
