39290acdb181e1fb1b4ba6496b0d8e5465d1a34d
Jesse Junior <jejunior@support.connectionsAcademy.org>
2024-12-16 17:12:56 -0300

    added missing migration

---
+++ ConWaysGame.Web/Migrations/20241216201239_final.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConWaysGame.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class final : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TotalGridCeels",
+                table: "Games",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalGridCeels",
+                table: "Games");
+        }
+    }
+}
